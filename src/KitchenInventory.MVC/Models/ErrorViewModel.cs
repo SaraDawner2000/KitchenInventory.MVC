aa8fae723808d53abb88816de420baba044ed088
natadem2013 <83845798+SaraDawner2000@users.noreply.github.com>
2025-02-20 14:36:00 -0600

    Initial commit

---
+++ src/KitchenInventory.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KitchenInventory.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ tests/KitchenInventory.MVC.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace KitchenInventory.MVC.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
